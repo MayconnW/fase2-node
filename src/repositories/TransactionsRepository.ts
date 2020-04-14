bc336db515b3a1d45a6153431b7d99543f12744c
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-04-14 11:55:52 -0300

    Write the code to pass all the tests

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateProps {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,32 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce(
+      (total, item) => total + (item.type === 'income' ? item.value : 0),
+      0,
+    );
+    const outcome = this.transactions.reduce(
+      (total, item) => total + (item.type === 'outcome' ? item.value : 0),
+      0,
+    );
+    const balance: Balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateProps): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
